652f8257560de0f2d76347748954dc574b11531b
HemantCrafts <mhemant2201@gmail.com>
2026-05-23 20:17:38 +0530

    Commenting the else block of server

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,10 @@ if (process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) =>
         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
     );
-} else {
-    app.get('/', (req, res) => {
-        res.send('API is running....');
-    });
+// } else {
+//     app.get('/', (req, res) => {
+//         res.send('API is running....');
+//     });
 }
 
 app.use(notFound);
